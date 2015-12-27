23849fff2cd3119a0430503a00260cf2e912b9e5
Puteulanus <mj@puteulanus.com>
2015-12-27 19:37:00 +0800

    init 7

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM centos:centos6
+FROM centos:centos7
 
 # Add scripts
 ADD script /tmp/script
--- script/source-update.sh
+++ script/source-update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # PHP
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 sed -i -e '/\[remi\]/,/^\[/s/enabled=0/enabled=1/' /etc/yum.repos.d/remi.repo
 sed -i -e '/\[remi\-php56\]/,/^\[/s/enabled=0/enabled=1/' /etc/yum.repos.d/remi.repo
 
@@ -10,7 +10,7 @@ sed -i -e '/\[remi\-php56\]/,/^\[/s/enabled=0/enabled=1/' /etc/yum.repos.d/remi.
 cat << _EOF_ >/etc/yum.repos.d/MariaDB.repo
 [mariadb]
 name = MariaDB
-baseurl = http://yum.mariadb.org/10.0/centos6-amd64
+baseurl = http://yum.mariadb.org/10.0/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 _EOF_
@@ -19,7 +19,7 @@ _EOF_
 cat << _EOF_ >/etc/yum.repos.d/nginx.repo
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/centos/6/x86_64/
+baseurl=http://nginx.org/packages/centos/7/x86_64/
 gpgcheck=0
 enabled=1
 _EOF_
