dbc03ebdf7e23956e155fac38e77034fc91a8419
Puteulanus <mj@puteulanus.com>
2015-12-27 17:45:37 +0800

    修正启动参数

--- script/php-install.sh
+++ script/php-install.sh
@@ -5,3 +5,6 @@ yum install -y php-fpm php-cli php-mysql php-gd php-imap \
 php-ldap php-odbc php-pear php-xml php-xmlrpc \
 php-magickwand php-mbstring php-mcrypt php-devel \
 php-mssql php-shout php-snmp php-soap php-tidy
+
+# Configure
+sed -i "s/daemonize = yes/daemonize = no/" /etc/php-fpm.conf
--- script/run-when-start.sh
+++ script/run-when-start.sh
@@ -5,6 +5,7 @@ yum install python-setuptools -y
 easy_install supervisor
 
 # Configure
+mkdir -p /var/log/mysql
 mkdir /etc/supervisord.d
 echo_supervisord_conf > /etc/supervisord.conf
 echo '[include]' >> /etc/supervisord.conf
@@ -15,10 +16,11 @@ cat << _EOF_ >"/etc/supervisord.d/nginx-php.ini"
 command=/usr/sbin/php-fpm -c /etc/php-fpm.conf
 
 [program:nginx]
-command=/usr/sbin/nginx
+command=/usr/sbin/nginx -g "daemon off;"
 _EOF_
 
 cat << _EOF_ >"/etc/supervisord.d/mysql.ini"
 [program:mysqld]
-command=/usr/sbin/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --user=mysql --log-error=/var/log/mysql/error.log --pid-file=/var/run/mysqld/mysqld.pid --socket=/var/run/mysqld/mysqld.sock --port=3306
+command=/usr/bin/pidproxy /var/run/mysqld/mysqld.pid /usr/bin/mysqld_safe --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --log-error=/var/log/mysql/error.log --socket=/var/run/mysqld/mysqld.sock --port=3306
+user=mysql
 _EOF_
